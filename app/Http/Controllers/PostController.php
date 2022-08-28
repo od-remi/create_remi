0506aee50448bc8f377286d7b790f7986ccc744c
od-remi <remisougo@gmail.com>
2022-08-28 18:26:30 +0900

    記事編集画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,6 +20,17 @@ public function create()
 {
     return view('posts/create');
 }
+public function edit(Post $post)
+{
+    return view('posts/edit')->with(['post'=>$post]);
+}
+public function update(PostRequest $request, Post $post)
+{
+    $input_post=$request['post'];
+    $post->fill($input_post)->save();
+    
+    return redirect('/posts/' . $post->id);
+}
 }
 
 
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', 'PostController@index');
 Route::get('/posts/create', 'PostController@create');
 
 Route::get('/posts/{post}', 'PostController@show');
+
+Route::get('/posts/{posts}/edit', 'PostController@edit');
+
+Route::put('/posts/{post}', 'PostController@update');
